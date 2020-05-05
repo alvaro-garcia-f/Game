4402d5432713ab607c1d5912b6380fbd5f9c55af
Álvaro García <alvaro.garcia.f@gmail.com>
2020-05-05 13:33:32 +0100

    Uses canvas element. Loads background as canvas style, character as green rectangle, ground as grey rectangle

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,1 +1,8 @@
-var playerSprite = document.getElementById("playerSprite");
+const canvas = document.getElementById("screen");
+const ctx = canvas.getContext("2d");
+
+function loadGround () {
+    ctx.beginPath();
+    ctx.fillStyle = "#2c2b40";
+    ctx.fillRect(0,498, 1000, 64);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,22 @@
+const GROUND = 498;
+
+// Player Class      
 var Player = function () {
-    this.x = 64;
+    const self = this;
+    this.h = 40;        // Player height
+    this.w = 32;        // Player width
+    this.x = 64;        // Starting horizontal position
+    this.y = GROUND - this.h;    // Starting vertical position
+    const MAX_JUMP_HEIGHT = GROUND + this.h; 
+    this.timer;
     
+    this.loadSprite = function () {
+        ctx.beginPath();
+        ctx.fillStyle = "#00FF00";
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.closePath();
+    }
+
     this.moveLeft = function () {
         if (this.x - 10 > 0) {   // Prevents crossing left border 
             this.x -= 10;
@@ -14,13 +30,44 @@ var Player = function () {
             playerSprite.style.left = `${this.x}px`;
         }
     }
+
+    this.jump = function () {
+        if (this.y < MAX_JUMP_HEIGHT) {
+            this.y += 10;
+            playerSprite.style.bottom = `${this.y}px`;
+        } else {
+            this.timer = setInterval(self.land, 100);
+        }
+    }
+
+    this.land = function () {
+        if (this.y >= GROUND && this.y < 10) {
+            this.y = GROUND;
+            playerSprite.style.bottom = `${this.y}px`;
+            clearInterval(this.timer);
+        } else {
+            this.y -= 10;
+            playerSprite.style.bottom = `${this.y}px`;
+        }
+    }
 };
 
+var Game = function () {
+
+    this.init = function () {
+        loadGround ();
+    }
+}
+
+var game = new Game ();
+game.init();
 
 var player = new Player();
+player.loadSprite();
 
+
+/*
 window.addEventListener("keydown", (e) => {
-    console.log(player);
     switch (e.key) {
         case "ArrowLeft":
             player.moveLeft();
@@ -28,5 +75,18 @@ window.addEventListener("keydown", (e) => {
         case "ArrowRight":
             player.moveRight();
             break;
+        case "ArrowUp":
+            player.jumping = true;
+            player.jump();
+            break;
     }
 });
+
+window.addEventListener("keyup", (e) => {
+    console.log("up");
+    switch (e.key) {
+        case "ArrowUp":
+            player.land();
+            break;
+    }
+});*/

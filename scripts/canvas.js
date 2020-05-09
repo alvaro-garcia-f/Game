b6dec4bbd73dc685ff4e946810d9286e617e0a3d
Álvaro García <alvaro.garcia.f@gmail.com>
2020-05-09 16:09:23 +0100

    Drawing counters. Pending call

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -11,8 +11,15 @@ function drawGround() {
     ctx.fillRect(0, GROUND, SCR_WIDTH, 64);
 }
 
-function drawCounters() {
+function drawCounters(attempts, heart, time, clock) {
+    for (let i = 1; i <= attempts; i++) {
+        ctx.drawImage(heart, 25*i, 50, heart.width/2, heart.height/2);
+    }
 
+    ctx.drawImage(clock, 850, 50, clock.width/1.5, clock.height/1.5);
+    ctx.font = '20px Roboto';
+    ctx.fillStyle = "#fff";
+    ctx.fillText(`${time}s`, 890, 73);
 }
 
 function drawPlayer(player, image) {

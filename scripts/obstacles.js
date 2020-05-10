dd4925f2d0c3ac745bba690862f0b5eb9188d6ff
Álvaro García <alvaro.garcia.f@gmail.com>
2020-05-10 15:31:42 +0100

    Random distance between obstacles implemented.

--- scripts/obstacles.js
+++ scripts/obstacles.js
@@ -19,7 +19,7 @@ var ObstacleBuffer = function () {
         box1: {
             h: 24,
             w: 25,
-            sprite: "o1_box.png" 
+            sprite: "o1_box.png"
         },
         box2: {
             h: 32,
@@ -42,41 +42,44 @@ var ObstacleBuffer = function () {
     }
 
     this.createObstacle = function () {
-      //  this.bufferFront.push(this.createBox());
- 
-      if (this.bufferFront.length === 0 || 
-        this.bufferFront[this.bufferFront.length - 1].x + this.bufferFront[this.bufferFront.length - 1].w < 1000) { this.createBox(SCR_WIDTH); }
-      else {
-          this.createBox(this.bufferFront[this.bufferFront.length - 1].x + this.bufferFront[this.bufferFront.length - 1].w) }
+        //  this.bufferFront.push(this.createBox());
+
+        if (this.bufferFront.length === 0 ||
+            this.bufferFront[this.bufferFront.length - 1].x +
+            this.bufferFront[this.bufferFront.length - 1].w < 1000) { this.createBox(SCR_WIDTH); }
+        else {
+            this.createBox(this.bufferFront[this.bufferFront.length - 1].x +
+            this.bufferFront[this.bufferFront.length - 1].w + Math.round(Math.random()*300) + 100);
+        }
     }
 
     this.sendObstacleBack = function () {
         this.bufferBack.push(this.bufferFront.shift());
     }
-    
+
     this.sendObstacleForward = function () {
         this.bufferFront.unshift(this.bufferBack.pop());
     }
 
     // Obstacle creators
     this.createBox = function (position) {
-        if (Math.random() < 0.5) { this.bufferFront.push(new Obstacle ("box1", position, OBSTACLES.box1.w, OBSTACLES.box1.h, OBSTACLES.box1.sprite)); }
-        else { this.bufferFront.push(new Obstacle ("box2", position, OBSTACLES.box2.w, OBSTACLES.box2.h, OBSTACLES.box2.sprite)); }
+        if (Math.random() < 0.5) { this.bufferFront.push(new Obstacle("box1", position, OBSTACLES.box1.w, OBSTACLES.box1.h, OBSTACLES.box1.sprite)); }
+        else { this.bufferFront.push(new Obstacle("box2", position, OBSTACLES.box2.w, OBSTACLES.box2.h, OBSTACLES.box2.sprite)); }
     }
 
     //Obstacle animation
     this.animateObstacles = function () {
-       if(this.bufferFront.length > 0) 
+        if (this.bufferFront.length > 0)
             this.bufferFront.forEach((obstacle) => {
-                obstacle.move();        
+                obstacle.move();
             });
 
-       if(this.bufferBack.length > 0)
-           this.bufferBack.forEach((obstacle) => {
-               obstacle.move();
-           });
-           
-       if(this.bufferBack.length && this.bufferBack[0].x + this.bufferBack[0].w <= 0) 
+        if (this.bufferBack.length > 0)
+            this.bufferBack.forEach((obstacle) => {
+                obstacle.move();
+            });
+
+        if (this.bufferBack.length && this.bufferBack[0].x + this.bufferBack[0].w <= 0)
             this.bufferBack.shift();
     }
 
